c5b45d90b693c73975c0010dc31d8f1d44cb6ba2
danilsadonenko <danilsadonenko@mail.ru>
2022-06-27 21:47:02 +0300

    Final

---
+++ FilesWatcher/FileWatcher.cs
@@ -0,0 +1,32 @@
+﻿using Beeline.FileHandler;
+using Beeline.Recorders;
+
+namespace Beeline;
+
+public class FileWatcher : IDisposable
+{
+    private FileSystemWatcher _watcher;
+    private IRecorder _recorder;
+
+    public FileWatcher(string path, IRecorder recorder)
+    {
+        _watcher = new FileSystemWatcher(path);
+        _recorder = recorder;
+        _watcher.NotifyFilter = NotifyFilters.FileName;
+        _watcher.EnableRaisingEvents = true;
+        _watcher.Created += OnCreated;
+    }
+
+    private void OnCreated(object sender, FileSystemEventArgs e)
+    {
+        var fileExtension = Path.GetExtension(e.FullPath);
+        var file = FileReader.GetFileAsString(e.FullPath);
+        var result = FileHandlerFactory.GetHandler(fileExtension).Process(file);
+        _recorder.Record(result.ToArray(), e.FullPath);
+    }
+
+    public void Dispose()
+    {
+        _watcher.Dispose();
+    }
+}
